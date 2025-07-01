864ac3738af56086bc9b7fd9ea0ee4ed08bed0e9
Rishikeshsrikvs <rishikesh@srikvstech.com>
2025-07-02 00:37:24 +0530

    sdn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
-
 import React from "react";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">Ordhek<span>Deen</span></div>
+      <div className="logo">
+        Ordhek<span>Deen</span>
+      </div>
+
       <nav>
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>FAQ</li>
-          <li>Guide</li>
-          <li>Contact</li>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#welcome">About Us</a></li>
+          <li><a href="#how-it-works">How It Works</a></li>
+          <li><a href="#reviews">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
+
       <div className="auth-buttons">
         <button className="register">Registration</button>
         <button className="signin">Sign In</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hero from '../components/Hero';
 import CreateBiodata from '../components/CreateBiodata';
 import HowItWorks from '../components/HowItWorks';
@@ -9,23 +10,46 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import CustomerReviews from '../components/CustomerReviews';
 
-
 function Home() {
   return (
     <div>
-      <Header/>
-      <Hero />
-      <WelcomeSection />
-      
-      <HowItWorks />
-      <CustomerReviews />
-      
-      {/* <Testimonials /> */}
-      <WhyChooseUs />
-      <Gallery />
-      <CreateBiodata/>
-      
-      <Footer />
+      <Header />
+
+      <section id="hero">
+        <Hero />
+      </section>
+
+      <section id="welcome">
+        <WelcomeSection />
+      </section>
+
+      <section id="how-it-works">
+        <HowItWorks />
+      </section>
+
+      <section id="reviews">
+        <CustomerReviews />
+      </section>
+
+      {/* <section id="testimonials">
+        <Testimonials />
+      </section> */}
+
+      <section id="why-choose-us">
+        <WhyChooseUs />
+      </section>
+
+      <section id="gallery">
+        <Gallery />
+      </section>
+
+      <section id="create-biodata">
+        <CreateBiodata />
+      </section>
+
+      <section id="contact">
+        <Footer />
+      </section>
     </div>
   );
 }
